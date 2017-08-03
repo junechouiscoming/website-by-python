b774000112fa9de206435c7d25385221303382e8
junechou <junechouzz@foxmail.com>
2017-08-03 14:28:52 +0800

    最后一次提交，基本功能已完成

--- my_site/settings.py
+++ my_site/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # 我的网站
     'my_sites',
     'users',
+    'bootstrap3',
 
 ]
 
@@ -123,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# 我的设置
+LOGIN_URL = '/users/login/'
+
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
--- my_sites/views.py
+++ my_sites/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -11,21 +12,25 @@ def index(request):
     return render(request, 'my_sites/index.html')
 
 
+@login_required
 def topics(request):
     """显示所有主题"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'my_sites/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有条目"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'my_sites/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
@@ -33,16 +38,20 @@ def new_topic(request):
     else:
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('my_sites:topics'))
 
     context = {'form': form}
     return render(request, 'my_sites/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """添加新条目"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         form = EntryForm()
@@ -55,14 +64,15 @@ def new_entry(request, topic_id):
             return HttpResponseRedirect(reverse('my_sites:topic', args=[topic_id]))
 
     context = {'topic': topic, 'form': form}
-
     return render(request, 'my_sites/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """编辑既有的条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
@@ -74,3 +84,9 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'my_sites/edit_entry.html', context)
+
+
+# 检测登录用户是否是请求用户，如果不是，返回404
+def check_topic_owner(topic, request):
+    if topic.owner != request.user:
+        raise Http404
